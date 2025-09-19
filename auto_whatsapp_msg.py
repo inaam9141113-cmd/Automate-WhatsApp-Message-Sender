5e05bd14971f5cde1495fa188875a4eb07de63b3
inaam9141113-cmd <inaam9141113@gmail.com>
2025-09-19 16:22:57 -0700

    Create auto_whatsapp_msg.py

---
+++ auto_whatsapp_msg.py
@@ -0,0 +1,8 @@
+import pywhatkit
+
+pywhatkit.sendwhatmsg("+1234567890", "Hello from Python!", 16, 18)
+
+# In the numbering place write the number to which you want to send the message.
+# In the time place write the time at which you want to send the message.
+# The time should be in 24-hour format.
+# Number 16 is the hours and 18 is the minutes.
